4ba3d4737a63cf213401696c523593e4cda0ec86
luis <lrosario101@gmail.com>
2014-02-26 18:42:31 -0500

    initial commit

---
+++ GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Leap;
+
+/*
+
+this code controls the whole experience
+
+*/
+
+public class GameManager : MonoBehaviour {
+
+//	public string location;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// RIPPLE ENGAGE
+		Debug.Log("RIPPLE");
+		Debug.Log ("=======================");
+
+
+		// determine location
+		// area1 - start
+		// area2 - acidification
+		// area3 - pollution
+		// area4 - over-fishing
+//		location = "area1";
+//		Debug.Log (location);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}	
+}
---
+++ SphereTrigger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereTrigger : MonoBehaviour {
+
+	SphereTrigger sphereWorldOneObj;
+	SphereTrigger sphereWorldTwoObj;
+
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Sphere Trigger Script");
+		sphereWorldOneObj = GameObject.Find("sphereWorldOne").GetComponent<SphereTrigger>();
+		sphereWorldTwoObj = GameObject.Find("sphereWorldTwo").GetComponent<SphereTrigger>();
+		//Debug.Log("What is this: " + sphereWorldOneObj);
+		//sphereWorldOne = GameObject.Find("sphereWorldOne");
+		//Debug.Log("sphereWorldOne: " + sphereWorldOne);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider objectThatWasHit)
+	{
+
+		//Debug.Log("Shit went down. The Collision object is: " + objectThatWasHit + this);
+		if (this == sphereWorldOneObj) 
+		{
+			Debug.Log("I'm in Sphere World One "  + this);
+
+		}
+
+		if (this == sphereWorldTwoObj) 
+		{
+			Debug.Log("I'm in Sphere World Two "  + this);
+			//GUI.Label(new Rect(35, 60, 150, 100), "Location: 2");
+			if(!audio.isPlaying)
+			{
+				audio.Play();
+			}
+		}
+	}
+
+}
